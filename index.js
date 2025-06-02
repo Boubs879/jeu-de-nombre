19c2b138d8dd6291b4d3520e8510b02132e6d6fc
Boubs879 <ngomb4309@gmail.com>
2025-06-02 11:20:58 -0700

    ajout du nombre de tentative

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const bouton = document.querySelector(".click");
-
+let nombreTentative = 0;
 // Génère un nombre entre 1 et 100
-const secretNumber = Math.floor(Math.random() * 100) + 1;
+const secretNumber = Math.floor(Math.random() * 100);
 console.log("Nombre secret :", secretNumber);
 
 bouton.addEventListener("click", () => {
-    let reponse = parseInt(prompt("Devine un nombre entre 1 et 100 :"));
+    let reponse = prompt("Devine un nombre entre 1 et 100 :");
 
     while (reponse !== secretNumber) {
+        nombreTentative++;
         if (reponse > secretNumber) {
             alert("Trop grand !");
         } else if (reponse < secretNumber) {
@@ -18,4 +19,6 @@ bouton.addEventListener("click", () => {
     }
 
     alert("Bravo ! Tu as trouvé le bon nombre !");
+    alert("Bravo ! Tu as trouvé en " + nombreTentative + " tentative(s).");
+
 });
